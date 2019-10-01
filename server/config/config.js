7b3610fd0e9225508912b71242350c324242543e
lukeherczeg <lukeherczeg@ufl.edu>
2019-10-01 09:31:53 -0400

    Refresh repo

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://guest:guest@test-sprtr.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    },
-    openCage: {
-        key: 'f8ea6e9809414a86b6b4359abbfe343d' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
-    },
-    port: 8080
-};
